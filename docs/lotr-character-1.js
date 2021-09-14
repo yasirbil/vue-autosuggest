a1e0e670ae1f20978b27a28fb0238f47f2c51c9c
Yasir Bilgin <51059377+yasirbil@users.noreply.github.com>
2021-09-13 21:08:59 -0500

    Create lotr-character-1.js

---
+++ docs/lotr-character-1.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    "Name": "Adanalidir",
+    "Url": "http://lotr.wikia.com//wiki/Adanalidir",
+    "Race": "Human"
+  },
+  {
+    "Name": "Adrahil I",
+    "Url": "http://lotr.wikia.com//wiki/Adrahil_I",
+    "Race": "Human"
+  }
+]
